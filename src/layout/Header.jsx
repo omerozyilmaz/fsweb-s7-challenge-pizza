8e932f384ed6b44f997b86da04bdea0302f02702
Ömer ÖZYILMAZ <omerozylmaz2@gmail.com>
2024-05-21 16:38:13 +0300

    Update

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,18 +1,22 @@
-import '../Layout.css'; 
-import { useLocation } from 'react-router-dom';
+import "../Layout.css";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
-    const location = useLocation();
+  const location = useLocation();
 
-    return (
-        <header>
-            <nav className='main-nav'>
-                <div className='nav-container'>
-                    <a href="/" className='headerNav'>
-                        <img src="./Assets/mile1-assets/logo.svg" alt="logo" style={{ marginTop: 30, marginBottom: 10 }} />
-                    </a>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <nav className="main-nav">
+        <div className="nav-container">
+          <a href="/" className="headerNav">
+            <img
+              src="./Assets/mile1-assets/logo.svg"
+              alt="logo"
+              style={{ marginTop: 30, marginBottom: 10 }}
+            />
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 }
--- src/pages/OrderForm.jsx
+++ src/pages/OrderForm.jsx
@@ -1,82 +1,235 @@
-import React, { useState } from "react";
-import PizzaPrice from "../components/PizzaPrice";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function OrderForm() {
-    const [selectedSize, setSelectedSize] = useState('');
-    const [selectedDough, setSelectedDough] = useState('');
-    const [additionalMaterial, setAdditionalMaterial] =useState([]);
-    const materials = ["Pepperoni", "Sosis", "Tavuk", "Soğan", "Domates", "Mısır", "Sarımsak", "Biber", "Jalapeno", "Sucuk", "Ananas", "Kabak"];
+  const [selectedSize, setSelectedSize] = useState("L");
+  const [selectedDough, setSelectedDough] = useState("");
+  const [additionalMaterial, setAdditionalMaterial] = useState([]);
+  const [pizzaCount, setPizzaCount] = useState(1);
+  const [price, setPrice] = useState({
+    aciPizzaPrice: 85.5,
+    totalPrice: 85.5,
+    additionalMaterialsPrice: 0,
+  });
+  const [errorMaterial, setErrorMaterial] = useState(false);
 
-    const handleMaterialChange = (material) =>{
+  const materials = [
+    "Pepperoni",
+    "Sosis",
+    "Tavuk",
+    "Soğan",
+    "Domates",
+    "Mısır",
+    "Sarımsak",
+    "Biber",
+    "Jalapeno",
+    "Sucuk",
+    "Ananas",
+    "Kabak",
+  ];
 
-        setAdditionalMaterial(prev => prev.includes(material) ? prev.filter(i => !material): [...prev, material]);
+  const handleCountChange = (event) => {
+    if (event.target.name === "increase") {
+      setPizzaCount((prevCount) => prevCount + 1);
+    } else if (event.target.name === "decrease") {
+      setPizzaCount((prevCount) => (prevCount > 1 ? prevCount - 1 : 1));
+    }
+  };
+
+  const handleMaterialChange = (material) => {
+    setAdditionalMaterial((prev) =>
+      prev.includes(material)
+        ? prev.filter((i) => i !== material)
+        : [...prev, material]
+    );
+  };
+
+  useEffect(() => {
+    const additionalMaterialsPrice = additionalMaterial.length * 5 * pizzaCount;
+    const totalPrice =
+      (price.aciPizzaPrice + additionalMaterialsPrice) * pizzaCount;
+    setPrice((firstPrice) => ({
+      ...firstPrice,
+      totalPrice,
+      additionalMaterialsPrice,
+    }));
+    setErrorMaterial(additionalMaterial.length < 4);
+  }, [additionalMaterial, pizzaCount, price.aciPizzaPrice]);
+
+  const handleSubmit = async () => {
+    const data = {
+      selectedDough,
+      selectedSize,
+      additionalMaterial,
+      pizzaCount,
+      totalPrice: price.totalPrice,
     };
 
-    return (
-        <>
-        <div className="container topColor">
-            <div className="orderFormSecondDiv">
-                <div className="">
-                    <img id="formBanner" src="src/assets/mile2-aseets/pictures/form-banner.png" alt="Form Banner"/>
-                    <div className='nav-links'>
-                        <a href="/" style={{ color: 'black' }}>Anasayfa</a>
-                        <span style={{ color: 'black' }}> - </span>
-                        <a href="/OrderForm" style={{ color: 'red', fontWeight: "bold" }}>Sipariş Oluştur</a>
-                    </div>
-                <div style={{fontWeight:800, paddingBottom:50, paddingTop:50, fontSize:22}}>Position Absolute Acı Pizza</div>
-                <PizzaPrice />
-                <div style={{paddingBottom:30, paddingTop:30}}>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.</div>
-                </div>
-                </div>
-        </div>
-        <div className="container orderFormSecondDiv">
-                <div className="selection-section">
-                    <div className="option-group">
-                        <label className="option-label">Boyut Seç <span className="required">*</span></label>
-                        <div className="options">
-                            <button className={`option ${selectedSize === 'S' ? 'selected' : ''}`} onClick={() => setSelectedSize('S')}>S</button>
-                            <button className={`option ${selectedSize === 'M' ? 'selected' : ''}`} onClick={() => setSelectedSize('M')}>M</button>
-                            <button className={`option ${selectedSize === 'L' ? 'selected' : ''}`} onClick={() => setSelectedSize('L')}>L</button>
-                        </div>
-                    </div>
-                    <div className="option-group">
-                        <label className="option-label">Hamur Seç <span className="required">*</span></label>
-                        <select style={{backgroundColor:' #FAF7F2'}} value={selectedDough} onChange={(e) => setSelectedDough(e.target.value)}>
-                            <option value="">--Hamur Kalınlığı Seç--</option>
-                            <option value="thin">İnce</option>
-                            <option value="thick">Kalın</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="additional-material-section">
-                    <label className="option-label">Ek Malzemeler</label>
-                    <div style={{paddingTop:20}}>En Fazla 10 Malzeme Seçebiliriniz. 5₺</div>
-                    <div className="additional-material-option">
-                        {materials.map(material => (
-                            <label key={material} className="additional-material-option">
-                                <input
-                                type="checkbox"
-                                value={material}
-                                checked = {additionalMaterial.includes(material)}
-                                onChange={()=> handleMaterialChange(material)}    
-                                />
-                                {material}
-                            </label>
-                        ))}
-                    </div>
-                </div>
-                <div className="order-summary">
-                    <div className="order-note">
-                        <label>Sipariş Notu</label>
-                        <textarea placeholder="Siparişe eklemek istediğiniz bir not var mı?" />
-                    </div>
-                    <div className="order-total">
-                        <div>Seçimler: 25.00₺</div>
-                        <div>Toplam: 110.50₺</div>
-                        <button>SİPARİŞ VER</button>
-                    </div>
-                </div>
+    try {
+      const response = await axios.post("https://reqres.in/api/pizza", data);
+      console.log("Order Summary:", response.data);
+    } catch (error) {
+      console.error("There was an error submitting the order: ", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="container topColor">
+        <div className="orderFormSecondDiv">
+          <div className="">
+            <img
+              id="formBanner"
+              src="src/assets/mile2-aseets/pictures/form-banner.png"
+              alt="Form Banner"
+            />
+            <div className="nav-links">
+              <a href="/" style={{ color: "black" }}>
+                Anasayfa
+              </a>
+              <span style={{ color: "black" }}> - </span>
+              <a href="/OrderForm" style={{ color: "red", fontWeight: "bold" }}>
+                Sipariş Oluştur
+              </a>
             </div>
-        </>
-    );
+            <div
+              style={{
+                fontWeight: 800,
+                paddingBottom: 50,
+                paddingTop: 50,
+                fontSize: 22,
+              }}
+            >
+              Position Absolute Acı Pizza
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <div style={{ fontWeight: 900, fontSize: 27 }}>
+                {price.aciPizzaPrice}₺
+              </div>
+              <div>
+                4.9 <span style={{ marginLeft: "10px" }}>(200)</span>
+              </div>
+            </div>
+            <div style={{ paddingBottom: 30, paddingTop: 30 }}>
+              Frontent Dev olarak hala position:absolute kullanıyorsan bu çok
+              acı pizza tam sana göre. Pizza, domates, peynir ve genellikle
+              çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak
+              odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle
+              yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan
+              İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen
+              pizzetta denir.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container  orderFormSecondDiv">
+        <div className="selection-section">
+          <div className="option-group">
+            <label className="option-label">
+              Boyut Seç <span className="required">*</span>
+            </label>
+            <div className="options">
+              <button
+                className={`option ${selectedSize === "S" ? "selected" : ""}`}
+                onClick={() => setSelectedSize("S")}
+              >
+                S
+              </button>
+              <button
+                className={`option ${selectedSize === "M" ? "selected" : ""}`}
+                onClick={() => setSelectedSize("M")}
+              >
+                M
+              </button>
+              <button
+                className={`option ${selectedSize === "L" ? "selected" : ""}`}
+                onClick={() => setSelectedSize("L")}
+              >
+                L
+              </button>
+            </div>
+          </div>
+          <div className="option-group">
+            <label className="option-label">
+              Hamur Seç <span className="required">*</span>
+            </label>
+            <select
+              style={{ backgroundColor: " #FAF7F2" }}
+              value={selectedDough}
+              onChange={(e) => setSelectedDough(e.target.value)}
+            >
+              <option value="">--Hamur Kalınlığı Seç--</option>
+              <option value="ince">İnce</option>
+              <option value="kalin">Kalın</option>
+            </select>
+          </div>
+        </div>
+        <div className="additional-material-section">
+          <label className="option-label">Ek Malzemeler</label>
+          <div style={{ paddingTop: 20 }}>
+            En Fazla 10 Malzeme Seçebiliriniz. 5₺
+          </div>
+          <div className="additional-material-option">
+            {materials.map((material) => (
+              <label key={material} className="additional-material-option">
+                <input
+                  type="checkbox"
+                  value={material}
+                  checked={additionalMaterial.includes(material)}
+                  onChange={() => handleMaterialChange(material)}
+                />
+                {material}
+              </label>
+            ))}
+          </div>
+          {errorMaterial && (
+            <div style={{ color: "red" }}>
+              Lütfen en az 4 tane ek malzeme seçiniz.
+            </div>
+          )}
+        </div>
+        <div className="order-summary ">
+          <div className="selection-section">
+            <div className="order-note yanyana">
+              <label className="option-label ">Isim Soyisim</label>
+              <textarea placeholder="Isim Soyisim giriniz." />
+              <label className="option-label">Sipariş Notu</label>
+              <textarea placeholder="Siparişe eklemek istediğiniz bir not var mı?" />
+            </div>
+          </div>
+          <hr />
+          <div className="selection-section">
+            <div className="yanyana">
+              <button
+                className="yellow buttonScale"
+                name="decrease"
+                onClick={handleCountChange}
+              >
+                -
+              </button>
+              <div className="buttonScale center">{pizzaCount}</div>
+              <button
+                className="yellow buttonScale"
+                name="increase"
+                onClick={handleCountChange}
+              >
+                +
+              </button>
+            </div>
+            <div className="order-total option-group">
+              <div>Sipariş Toplamı</div>
+              <a>Seçimler: {price.additionalMaterialsPrice}₺</a>
+              <a className="colorA">Toplam: {price.totalPrice}₺</a>
+              <button
+                disabled={additionalMaterial.length < 4}
+                onClick={handleSubmit}
+              >
+                SİPARİŞ VER
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
